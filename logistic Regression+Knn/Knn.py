f6afc249a06b0da42d3cb3897e5410dfea25f82f
Tkfrnfl <rul64@naver.com>
2022-10-28 19:58:36 +0900

    complete knn

--- logistic Regression+Knn/Knn.py
+++ logistic Regression+Knn/Knn.py
@@ -6,57 +6,64 @@ from PIL import Image
 
 class KNN():
     K=15
-    #int_list28=[]
     dist_list=[]
-    test_num=11
+    test_num=0
     global_input_list=[]
     global_label_list=[]
 
-    def get_distance(input_list,label_list):
-        KNN.test_num=len(input_list)//100 #100번째 데이터를 테스트로 사용하므로
+    def get_distance(train_list,train_label,test_list,test_label):
+        # np_train=np.array(train_list)
+        # np_test=np.array(test_list)
 
-        np_input=np.array(input_list)
-        np_label=np.array(label_list)
-        KNN.global_label_list=np_label
+        KNN.test_num=len(test_list)
+        KNN.global_label_list=test_label
 
-        # print(np_input[0])
-        for idx, val in enumerate(np_input):
-            if(idx+1)%100==0: #10000개의 데이터중 100번째 데이터인 경우 테스트 데이터로 사용
+        for idx_test,val_test in enumerate(test_list):
+            for idx_train,val_train in enumerate(train_list):
+                dist=0
+                for i in range(0,28):
+                    for j in range(0,28):
+                        dist+=math.pow(val_test[i][j]-val_train[i][j],2)
 
-                for idx_10000,val_10000 in enumerate(np_input):
-                    
-                    if (idx_10000+1)%100!=0:    
-                        tmp_list=np.power(np_input[idx_10000]-np_input[idx],2)
+                KNN.dist_list.append([sqrt(dist),train_label[idx_train]])
 
-                        # for i in range(0,28):
-                        #     for j in range(0,28):
-                        #       dist+=math.pow((val[i][j]-val_10000[i][j]),2)
-                    KNN.dist_list.append([sqrt(np.sum(tmp_list)),label_list[idx_10000]])   #거리,라벨 저장
 
-        #print(KNN.dist_list)
     def get_neighbors():
+        correct_count=0
         np_dist=np.array(KNN.dist_list)
-        sort_dist_list=np.split(np_dist,KNN.test_num)#앞에서부터 순서대로 0, 100...200 번째의 거리값
+        sort_dist_list=np.split(np_dist,KNN.test_num)  #앞에서부터 순서대로 1,2,3..번째 테스트 데이터와의 거리계산값
+
         
-        sort_dist_list=np.sort(sort_dist_list,axis=1)
-        # for i in range(len(sort_dist_list)):
-        #     np.sort(sort_dist_list[i],axis=1)
-        print(sort_dist_list[0])
+        for i in range(KNN.test_num):    
+            sort_dist_list[i]=sort_dist_list[i][sort_dist_list[i][:,0].argsort()]
+        
         no_wieght_list=[[0]*11 for _ in  range(100)]        # 이웃들의 클래스 저장해주는 리스트
-        
-        #print(sort_dist_list)
+        wieght_list=[[0]*11 for _ in  range(100)]        # 이웃들의 가중치 클래스 저장해주는 리스트
+
         for idx,val in enumerate(sort_dist_list):
             for i in range(0,KNN.K):
                 no_wieght_list[idx][int(val[i][1])]+=1
 
+        for idx,val in enumerate(sort_dist_list):
+            for i in range(0,KNN.K):
+                wieght_list[idx][int(val[i][1])]+=(1/val[i][0])
 
-        for i in range(KNN.test_num):
+        for i in range(KNN.test_num):                       #결과 출력
             print(str(no_wieght_list[i].index(max(no_wieght_list[i])))+' '\
-                +str(KNN.global_label_list[i*100]) )
+                +str(KNN.global_label_list[i]) )
+            if no_wieght_list[i].index(max(no_wieght_list[i]))==KNN.global_label_list[i]:
+                correct_count+=1
+        print('정확도'+str(correct_count/KNN.test_num))    
 
-        # print((no_wieght_list[3]))
-        # print(KNN.global_label_list[300])
-        #print(sort_dist_list[0])
-
+        print('--------next>>weighted--------------')
+        correct_count=0
+        for i in range(KNN.test_num):                       #결과 출력 (가중치)
+            print(str(wieght_list[i].index(max(wieght_list[i])))+' '\
+                +str(KNN.global_label_list[i]) )
+            if wieght_list[i].index(max(wieght_list[i]))==KNN.global_label_list[i]:
+                correct_count+=1
+        print('정확도'+str(correct_count/KNN.test_num))  
+    # def get_neighbors_weight():
+    #     print()
 
  
--- logistic Regression+Knn/hw2Knn.py
+++ logistic Regression+Knn/hw2Knn.py
@@ -4,9 +4,11 @@ import numpy as np
 from dataset.mnist import load_mnist 
 from PIL import Image 
 from Knn import KNN
+import time
+import math
 
 (x_train, t_train), (x_test, t_test) = \
- load_mnist(flatten=True, normalize=False) 
+ load_mnist(flatten=True, normalize=True) 
 
 # image = x_train[1]
 # #label = t_train[0] 
@@ -14,23 +16,36 @@ from Knn import KNN
 # #print(label) 
 # #print(image.shape)
 
-# def img_show(img): 
-#  pil_img = Image.fromarray(np.uint8(img)) 
-#  pil_img.show() 
+def img_show(img): 
+ pil_img = Image.fromarray(np.uint8(img)) 
+ pil_img.show() 
 # # image를 unsigned int로
 
+#img_show(test)
 # image = image.reshape(28,28) 
 # # 1차원 —> 2차원 (28x28) 
 # print(image) 
 # img_show(image)
 
-input_list=[]
-for idx, val in enumerate(x_train):
+train_list=[]
+test_list=[]
+for idx, val in enumerate(x_train):   #학습 리스트생성
     if idx<10000:
         val=val.reshape(28,28)
-        input_list.append(val)
-        # pil_img = Image.fromarray(np.uint8(val)) 
-        # input_list.append(pil_img.reshape(28,28))
-  
-KNN.get_distance(input_list,t_train)
+        train_list.append(val)
+
+for idx, val in enumerate(x_test):  #테스트 리스트 생성
+    if idx<100:
+        val=val.reshape(28,28)
+        test_list.append(val)
+
+start =time.time()  
+math.factorial(100000)
+KNN.get_distance(train_list,t_train,test_list,t_test)
+end_dist = time.time()
 KNN.get_neighbors()
+end_neighbors = time.time()
+print("Fit time: "+ f"{end_neighbors - start:.10f}")
+# KNN.get_neighbors_weight()
+# end_neighbors_weight = time.time()
+# print("Fit time: "+ f"{(end_dist-start)+(end_neighbors_weight-end_neighbors):.10f}")
